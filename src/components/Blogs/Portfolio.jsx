b5de26df3377c51c6c02c4a873ebff778c0e87ba
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-07-22 16:08:32 +0500

    portfolio update

--- src/components/Blogs/Portfolio.jsx
+++ src/components/Blogs/Portfolio.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogCard from "./BlogCard";
 import Img1 from "../../assets/blog/portfolio1.png";
 import Img2 from "../../assets/blog/portfolio2.png";
 import Img3 from "../../assets/blog/portfolio3.png";
+import Img4 from "../../assets/blog/portfolio4.png";
+import Img5 from "../../assets/blog/portfolio5.png";
+import Img6 from "../../assets/blog/portfolio6.png";
+import Img7 from "../../assets/blog/portfolio7.png";
+
 
 const BlogsData = [
   {
@@ -35,24 +40,84 @@ const BlogsData = [
     date: "April 22, 2022",
     link: "https://buddy.ai/"
   },
+  {
+    id: 4,
+    image: Img4,
+    title: "Health Care",
+    description:
+      "“The results have been incredible. With Power Digital, it feels like they’re in our trench, supporting and understanding us. They’re like a partner and mentor in helping us get where we want to be. “The results have been incredible. ",
+    author: "Someone",
+    date: "April 22, 2022",
+    link: "https://earlux.com/"
+  },
+  {
+    id: 5,
+    image: Img5,
+    title: "Business Development",
+    description:
+      "“The results have been incredible. With Power Digital, it feels like they’re in our trench, supporting and understanding us. They’re like a partner and mentor in helping us get where we want to be. “The results have been incredible. ",
+    author: "Someone",
+    date: "April 22, 2022",
+    link: "https://rooza.nl/en/"
+  },
+  {
+    id: 6,
+    image: Img6,
+    title: "Ecommerce Development",
+    description:
+      "“The results have been incredible. With Power Digital, it feels like they’re in our trench, supporting and understanding us. They’re like a partner and mentor in helping us get where we want to be. “The results have been incredible. ",
+    author: "Someone",
+    date: "April 22, 2022",
+    link: "https://fooji.com/"
+  },
+  {
+    id: 7,
+    image: Img7,
+    title: "WordPress Development",
+    description:
+      "“The results have been incredible. With Power Digital, it feels like they’re in our trench, supporting and understanding us. They’re like a partner and mentor in helping us get where we want to be. “The results have been incredible. ",
+    author: "Someone",
+    date: "April 22, 2022",
+    link: "https://thelex.live/?v=27889b81b317"
+  },
+  // {
+  //   id: 8,
+  //   image: Img3,
+  //   title: "WordPress Development",
+  //   description:
+  //     "“The results have been incredible. With Power Digital, it feels like they’re in our trench, supporting and understanding us. They’re like a partner and mentor in helping us get where we want to be. “The results have been incredible. ",
+  //   author: "Someone",
+  //   date: "April 22, 2022",
+  //   link: "https://buddy.ai/"
+  // }
 ];
 
 const Portfolio = () => {
+  const [visibleProjects, setVisibleProjects] = useState(3);
+
+  const handleViewMore = () => {
+    setVisibleProjects((prevVisible) => prevVisible + 3);
+  };
+
   return (
     <>
-      <div className="bg-gray-100 dark:bg-gray-900 dark:text-white py-10 pb-14" >
-        <section data-aos="fade-up" className="container ">
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-white py-10 pb-14">
+        <section data-aos="fade-up" className="container">
           <h1 className="my-8 border-l-8 border-primary/50 py-2 pl-2 text-3xl font-semibold">
             Some Our Works <span className="text-gradient text-[48px]">.</span>
           </h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" >
-            {BlogsData.map((item) => (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {BlogsData.slice(0, visibleProjects).map((item) => (
               <BlogCard key={item.id} {...item} />
             ))}
           </div>
-          <div className="text-center">
-            <button className="primary-btn">View All Posts</button>
-          </div>
+          {visibleProjects < BlogsData.length && (
+            <div className="text-center">
+              <button className="primary-btn" onClick={handleViewMore}>
+                View More Posts
+              </button>
+            </div>
+          )}
         </section>
       </div>
     </>
--- src/components/BrandsLogo/BrandsLogo.jsx
+++ src/components/BrandsLogo/BrandsLogo.jsx
@@ -8,9 +8,9 @@ import Brand4 from "../../assets/brands/4.png";
 const BrandsLogo = () => {
   return (
     <>
-      <div className="container  bg-gray-200 dark:bg-gray-900 dark:text-white h-[180px] sm:mt-0">
+      <div className="  bg-gray-200 dark:bg-gray-900 dark:text-white sm:mt-0 py-10 h-fit">
         <h1 className="text-center text-lg font-bold py-2">Powering next-gen companies</h1>
-        <div className=" flex  items-center justify-center gap-10 mt-6">
+        <div className=" flex items-center justify-center gap-10 mt-6 lg:flex-row sm:flex-col">
           <img src={Brand1} className="h-auto w-1/3  filter grayscale dark:invert dark:filter dark:brightness-0" alt="" />
           <img src={Brand2} className="h-auto w-1/6 filter grayscale dark:invert dark:filter dark:brightness-0" alt="" />
           <img src={Brand3} className="h-auto w-1/4  filter grayscale dark:invert dark:filter dark:brightness-0" alt="" />
